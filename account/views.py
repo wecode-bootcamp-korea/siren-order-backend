26ef5303718ca7818a4e4af30bb2ca2a4326408d
seung-jae <suengjae12@naver.com>
2019-07-15 21:00:12 +0900

    Account implementation_v1_190715
    Account 1차 구현
    
    settings.py
    bcrypt 추가
    
    views.py
    AcconuntView(회원가입) 1차 구현
    
    urls.py
    end pint = account
    
    requirements.txt 추가(bcrypt, pyJWT, requests)

--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
+import my_settings
 
-# Create your views here.
+from django.shortcuts import render
+from account.models   import User, Social, Login
+from django.views     import View
+from django.http      import JsonResponse
+
+
+class AccountView(View):
+    def post(self, request):
+        new_user_info    = json.loads(request.body)
+
+        if User.objects.filter(email = new_user_info['email']).exists():
+            return JsonResponse({'message' : '이미 존재하는 이메일입니다.'}, status = 400)
+
+        bytes_pw         = bytes(new_user_info['password'], 'utf-8')
+        hashed_pw        = bcrypt.hashpw(bytes_pw, bcrypt.gensalt())
+
+        user_info        = User(
+            email        = new_user_info['email'],
+            password     = hashed_pw.decode('UTF-8'),
+            name         = new_user_info['name'],
+            phone_number = new_user_info['phone_number'],
+        )
+        user_info.save()
+
+        return JsonResponse({'message':'회원가입이 완료되었습니다.'}, status=200)
