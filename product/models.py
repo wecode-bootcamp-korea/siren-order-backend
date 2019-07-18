419052abd920e61a174cccb1e7bb875136929756
박지훈 <fergith@naver.com>
2019-07-18 13:24:48 +0900

    change table type and change column name

--- product/models.py
+++ product/models.py
@@ -15,25 +15,24 @@ class Section(models.Model):
 
 class Drinks(models.Model):
     name = models.CharField(max_length=50)
-    en_name = models.CharField(max_length=50)
+    english_name = models.CharField(max_length=50)
     img_url = models.CharField(max_length=450) 
     only_ice = models.BooleanField(default=True)
-    oz_price = models.CharField(max_length=10, null=True)
-    short_price = models.CharField(max_length=10, null=True)
-    tall_price = models.CharField(max_length=10, null=True)
-    grande_price = models.CharField(max_length=10, null=True)
-    venti_price = models.CharField(max_length=10, null=True)
+    oz_price = models.IntegerField(default=0, null=True, blank=True)
+    short_price = models.IntegerField(default=0, null=True, blank=True)
+    tall_price = models.IntegerField(default=0, null=True, blank=True)
+    grande_price = models.IntegerField(default=0, null=True, blank=True)
+    venti_price = models.IntegerField(default=0, null=True, blank=True)
     description = models.CharField(max_length=400, null=True)
     condition = models.CharField(max_length=200, null=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)   
 
     class Meta:
         db_table = 'drinks'
 
 class Hot(models.Model):
     name = models.CharField(max_length=50)
-    en_name = models.CharField(max_length=50)
+    english_name = models.CharField(max_length=50)
     img_url = models.CharField(max_length=450)
     description = models.CharField(max_length=400, null=True)
     drink = models.ForeignKey(Drinks, on_delete=models.CASCADE)
@@ -43,38 +42,36 @@ class Hot(models.Model):
 
 class Foods(models.Model):
     name = models.CharField(max_length=50)
-    en_name = models.CharField(max_length=50)
+    english_name = models.CharField(max_length=50)
     img_url = models.CharField(max_length=450)
-    price = models.CharField(max_length=10)
+    price = models.IntegerField(default=0, null=True, blank=True)
     description = models.CharField(max_length=400, null=True)
     condition = models.CharField(max_length=200, null=True)
     option = models.BooleanField(default=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)   
 
     class Meta:
         db_table = 'foods'
 
 class Stuff(models.Model):
     name = models.CharField(max_length=50)
-    en_name = models.CharField(max_length=50)
+    english_name = models.CharField(max_length=50)
     img_url = models.CharField(max_length=450)
-    price = models.CharField(max_length=10)
+    price = models.IntegerField(default=0, null=True, blank=True)
     volume = models.CharField(max_length=4)
     option = models.CharField(max_length=20, null=True)
     description = models.CharField(max_length=400, null=True)
     condition = models.CharField(max_length=200, null=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)   
 
     class Meta:
         db_table = 'stuff'
 
 class Cake(models.Model):
     name = models.CharField(max_length=50)
-    en_name = models.CharField(max_length=50)
+    english_name = models.CharField(max_length=50)
     img_url = models.CharField(max_length=450)
-    price = models.CharField(max_length=10)
+    price = models.IntegerField(default=0, null=True, blank=True)
     size = models.CharField(max_length=10)
     weight = models.CharField(max_length=10)
     description = models.CharField(max_length=400, null=True)
